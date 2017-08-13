77cb6978aa2cb948ed76bdd0b16c8dac33c1e7fe
Michal <michal@hebeworks.com>
2017-08-13 16:00:45 +0100

    User can now add additional wall/floor/ceilings, ground floors & windows

--- app/components/certificate-component.js
+++ app/components/certificate-component.js
@@ -25,10 +25,10 @@ export default Ember.Component.extend({
   }),
 
   totalHeatLossDay: Ember.computed('roomInputsConfig.@each.value', function totalHeatLossDay() {
-    const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
-    if (reqField) {
-      return reqField.value;
-    }
+    // const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
+    // if (reqField) {
+    //   return reqField.value;
+    // }
     return 'N/A';
   }),
 
@@ -49,26 +49,26 @@ export default Ember.Component.extend({
   }),
 
   MWT: Ember.computed('roomInputsConfig.@each.value', function totalHeatLossDay() {
-    const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
-    if (reqField) {
-      return reqField.value;
-    }
+    // const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
+    // if (reqField) {
+    //   return reqField.value;
+    // }
     return 'N/A';
   }),
 
   oversizeFactor: Ember.computed('roomInputsConfig.@each.value', function totalHeatLossDay() {
-    const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
-    if (reqField) {
-      return reqField.value;
-    }
+    // const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
+    // if (reqField) {
+    //   return reqField.value;
+    // }
     return 'N/A';
   }),
 
   underfloorHeating: Ember.computed('roomInputsConfig.@each.value', function totalHeatLossDay() {
-    const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
-    if (reqField) {
-      return reqField.value;
-    }
+    // const reqField = this.get('roomInputsConfig').find(field => field.name = '<field name>');
+    // if (reqField) {
+    //   return reqField.value;
+    // }
     return 'N/A';
   }),
 
@@ -83,75 +83,5 @@ export default Ember.Component.extend({
   blendingValve: Ember.computed('siteInputsConfig.@each.value', function blendingValve() {
     return 'TO BE DEFINED';
   }),
-
-
-  // date: new Date(),
-
-  // certificateFields: Ember.computed('siteInputsConfig.@each.value', function certificateFields() {
-  //   return [
-  //     {
-  //       name: 'installationDate',
-  //       label: 'Date installation was commissioned',
-  //       date: new Date(),
-  //       type: 'pickADate',
-  //     },
-  //     {
-  //       name: 'typeOfHeatGen',
-  //       label: 'Type of heat generator (boiler/heat pump/other)',
-  //       options: [
-  //         'Heat Pump',
-  //         'Condensing Boiler',
-  //         'Other',
-  //       ],
-  //       value: '',
-  //       type: 'select',
-  //     },
-  //     {
-  //       name: 'heatLossCalcForDesign',
-  //       label: 'Heat loss calculator used for the design (must conform to UK National Annex of EN12831)',
-  //       value: 'BRE Calculation tool for design of low temperature domestic heating systems',
-  //       type: 'constant',
-  //     },
-  //     {
-  //       name: 'assumedDesignExTemp',
-  //       label: 'Assumed Design External Temperature (DET) for heat loss calculations',
-  //       type: 'siteInputValue',
-  //       relatedField: 'DETinC',
-  //     },
-  //     {
-  //       name: 'confirmMatch',
-  //       label: 'Confirm that assumed internal temperatures were as given in Table 2 of the guide*. If there are any exceptions they should be listed here.',
-  //       options: [
-  //         'Yes',
-  //         'No',
-  //       ],
-  //       type: 'select',
-  //     },
-  //     {
-  //       name: 'totalHeatLoss',
-  //       label: 'Total building heat loss on the design day, in kW',
-  //       value: 1.86, // to be changed to dynamic
-  //       type: 'constant',
-  //     },
-  //     {
-  //       name: 'typeOfHeating',
-  //       label: 'Has the installation has been designed for intermittent heating or continuous heating?',
-  //       type: 'siteInputValue',
-  //       relatedField: 'heatingDuration',
-  //     },
-  //     {
-  //       name: 'upliftFactor',
-  //       label: 'If the installation has been designed for intermittent heating, (a) the uplift factor applied to the building heat loss, and (b) the uplift factor applied to the heat generator',
-  //       value: Ember.computed(function upliftFactor() {
-  //         const heatingDuration = this.get('siteInputsConfig').find(field => field.name === 'heatingDuration');
-  //         if (heatingDuration === 'Intermittent') {
-  //           return '';
-  //         }
-  //         return 'N/A';
-  //       }),
-  //       type: 'computedProp',
-  //     }
-  //   ]
-  // }),
 
 });
